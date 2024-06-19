2c640317784a3ee192219a7eddc12abb83e4c15e
Gede rana Dewadatta <info.dewadatta@gmail.com>
2024-06-20 02:18:33 +0800

    test commit git

---
+++ src/test/java/Cars.java
@@ -0,0 +1,19 @@
+public class Cars {
+    public static void main(String[] args) {
+        String[] Cars = new String[15];
+
+        Cars[0] = "Mitsubishi";
+        Cars[1] = "Tesla";
+        Cars[2] = "Honda";
+        Cars[3] = "Nissan";
+        Cars[4] = "KIA";
+        Cars[5] = "Hyundai";
+
+        System.out.println("Content of cars :" + Cars[0]);
+        System.out.println("Content of cars :" + Cars[1]);
+        System.out.println("Content of cars :" + Cars[2]);
+        System.out.println("Content of cars :" + Cars[3]);
+        System.out.println("Content of cars :" + Cars[4]);
+        System.out.println("Content of cars :" + Cars[5]);
+    }
+}
